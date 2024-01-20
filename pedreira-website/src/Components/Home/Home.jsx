1a9aa437bf38238f972ce73b288f9a9b6bff60e5
ephimeteus <109197865+ephimeteus@users.noreply.github.com>
2024-01-20 04:34:29 -0300

    added images

--- pedreira-website/src/Components/Home/Home.jsx
+++ pedreira-website/src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <section className="home">
+        <section id="home">
             <h2>Welcome to Coffin Shop</h2>
             <p>Your source for high-quality coffins.</p>
             <button>Browse All</button>
--- pedreira-website/src/Components/WhoWeAre/WhoWeAre.jsx
+++ pedreira-website/src/Components/WhoWeAre/WhoWeAre.jsx
@@ -1,10 +1,14 @@
 import "./whoWeAre.css"
+import whoAreWeImage from '../../assets/small-girl.jpg';
 
 const WhoWeAre = () => {
     return (
-        <section className="who-we-are">
-            <h2>Who Are We</h2>
-            <p>Learn about our company and our commitment to providing the best coffins for our customers.</p>
+        <section id="who-we-are">
+            <aside>
+                <h2>Who Are We</h2>
+                <p>Learn about our company and our commitment to providing the best coffins for our customers.</p>
+            </aside>
+            <img src={whoAreWeImage} alt="Who Are We Image" />
         </section>
     )
 };
